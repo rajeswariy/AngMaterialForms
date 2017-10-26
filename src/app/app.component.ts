3df8acc91853b2e2b093f9212636301d2f2a8a3d
Rajeshwari Y <rajeshwariy@newtglobal.chn.local>
2017-10-26 13:27:18 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  colors=[
+  { id:1,name:'Red' },
+    { id:2,name:'Green' },
+    { id:3, name:'Blue' },
+  ]
+  color=2;
+  
+//  title = 'app';
+//  isChecked=true;
+//  onChange($event)
+//  {
+//    console.log($event);
+//  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule,MatRadioModule,MatSelectModule, MatInputModule } from '@angular/material';
+//import {MatRadioModule} from '@angular/material';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
